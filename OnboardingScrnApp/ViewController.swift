0d4963275341efbdad0513290449319220a0fb36
shaahid-ios <shaahid-ios@github.com>
2020-12-15 21:47:05 +0530

    first commit

---
+++ OnboardingScrnApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  OnboardingScrnApp
+//
+//  Created by Admin on 15/12/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var loginbtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginbtn.layer.cornerRadius = 8
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
